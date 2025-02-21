305e40c75329d10304654a5f24216f21282a4627
Alex Muirhead <a.muirhead@uq.net.au>
2025-02-21 17:56:58 +1000

    Simplfy grid to find the problem

--- radiation/more-grid/grid.lua
+++ radiation/more-grid/grid.lua
@@ -1,13 +1,14 @@
 -- a simple grid
 
-a = Vector3:new{x=0.0, y=0.0};
-b = Vector3:new{x=1.0, y=0.0};
-c = Vector3:new{x=1.0, y=10.0};
-d = Vector3:new{x=0.0, y=10.0};
+width = 3;
+height = 15;
 
-resolution = 10;
+a = Vector3:new{x=0.0, y=0.0};
+b = Vector3:new{x=10 , y=0.0};
+c = Vector3:new{x=10 , y=15};
+d = Vector3:new{x=0.0, y=15};
 
 patch = CoonsPatch:new{p00=a, p10=b, p11=c, p01=d};
-grid = StructuredGrid:new{psurface=patch, niv=resolution+1, njv=resolution*10};
+grid = StructuredGrid:new{psurface=patch, niv=width+1, njv=height+1};
 
 registerFluidGrid{grid=grid, fsTag="initial"};
